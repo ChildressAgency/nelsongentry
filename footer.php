c3af945e238b14878d8c55b02cf8661245873173
childressdev <jcampbell@childressagency.com>
2017-08-22 11:55:25 -0400

    fonts

--- footer.php
+++
@@ -1,9 +0,0 @@
-      <div class="footer">
-        <div class="container">
-          <p>&copy; <?php echo date('Y'); ?> Paid for and authorized by Friends of Jason Pelt<br />Website created by <a href="https://childressagency.com">The Childress Agency</a></p>
-        </div>
-      </div>
-    </div>
-    <?php wp_footer(); ?>
-  </body>
-</html>
