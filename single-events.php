177c076516ba2f6769b77b7a0f19c8cdd2e7d73d
childressdev <jcampbell@childressagency.com>
2017-08-23 10:54:44 -0400

    template file created

--- single-events.php
+++ single-events.php
@@ -5,7 +5,9 @@
       <?php if(have_posts()): while(have_posts()): the_post(); ?>
         <h1><?php the_title(); ?></h1>
         <?php the_content(); ?>
-      <?php endwhile; endif; ?>
+      <?php endwhile; else: ?>
+        <p>Sorry, page not found.</p>
+      <?php endif; ?>
     </section>
   </div>
 </main>
--- single.php
+++ single.php
@@ -3,6 +3,7 @@
   <div class="container">
     <section id="mainContent">
       <?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <h1><?php the_title(); ?></h1>
         <?php the_content(); ?>
       <?php endwhile; endif; ?>
     </section>
